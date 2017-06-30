2b8f8dd6ec9d2c36ee638d87766f5501a17c78e1
DaltonDev <lundgren.dev@gmail.com>
2017-06-30 17:01:26 +0200

    connection status fix

--- js/carService.js
+++ js/carService.js
@@ -4,14 +4,30 @@ define(['./template.js', './clientStorage.js'], function(template, clientStorage
     var apiUrlCar = apiUrlPath + 'car.php?carId=';
 
     function loadMoreRequest(){
+      fetchPromise().then(function(status){
+        document.getElementById("connection-status").innerHTML = status;
+        loadMore();
+      });
+    }
+
+    function fetchPromise(){
+      return new Promise(function(resolve, reject){
         fetch(apiUrlLatest + "?carId=" + clientStorage.getLastCarId()).then(function(response){
           return response.json();
         }).then(function(data){
           clientStorage.addCars(data.cars)
           .then(function(){
-            loadMore();
+              resolve("Online");
+            document.getElementById("connection-status").setAttribute("Style", "color: white; background: green");
           });
-        })
+        }).catch(function(e){
+          resolve("No connection");
+          document.getElementById("connection-status").setAttribute("Style", "color: white; background: red");
+        });
+        setTimeout(function(){
+          resolve("Slow connection");
+        }, 3000);
+      });
     }
 
     function loadMore(){
